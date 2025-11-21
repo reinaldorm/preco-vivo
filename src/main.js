336a254253be3a4d084c2ae8d05b3b5cc71aa6d3
reinaldohrm <naldo-mar@hotmail.com>
2025-11-21 20:19:19 -0300

    a

---
+++ src/main.js
@@ -0,0 +1,122 @@
+import './raw.css';
+import './main.scss';
+
+const motherboard = document.getElementById('motherboard');
+const gpu = document.getElementById('gpu');
+const cpu = document.getElementById('cpu');
+
+function createProductElement({ titleText, rawPriceText, finalPriceText, linkText, tag }) {
+  const productDiv = document.createElement('div');
+  const imageUrl = `https://cdn.pcbuildwizard.com/images/products/${tag}-1-m.webp`;
+  productDiv.className = 'product';
+
+  const img = document.createElement('img');
+  img.src = imageUrl;
+  img.className = 'thumb';
+
+  const infoDiv = document.createElement('div');
+  infoDiv.className = 'info';
+
+  const title = document.createElement('h2');
+  title.className = 'title';
+  title.textContent = titleText;
+
+  const priceDiv = document.createElement('div');
+  priceDiv.className = 'price';
+
+  const rawPrice = document.createElement('p');
+  rawPrice.className = 'raw';
+  rawPrice.textContent = 'R$ ' + rawPriceText;
+
+  const finalPrice = document.createElement('p');
+  finalPrice.className = 'final';
+  finalPrice.textContent = 'R$ ' + finalPriceText;
+
+  const linkElement = document.createElement('a');
+  linkElement.className = 'link';
+  linkElement.href = linkText;
+  linkElement.target = '_blank';
+  linkElement.textContent = 'Ir pra loja';
+
+  if (rawPriceText == finalPriceText) rawPrice.hidden = true;
+
+  // Assemble the structure
+  priceDiv.appendChild(rawPrice);
+  priceDiv.appendChild(finalPrice);
+  priceDiv.appendChild(linkElement);
+
+  infoDiv.appendChild(title);
+  infoDiv.appendChild(priceDiv);
+
+  productDiv.appendChild(img);
+  productDiv.appendChild(infoDiv);
+
+  return productDiv;
+}
+
+async function getFilteredMotherboard() {
+  const data = await fetch(
+    'https://api.pcbuildwizard.com/products/motherboards/recommend?merchants=&payment=&formFactors=&sockets=AM5&chipsets=B650%2CB850%2CX870&wifiStandards=&colors=&manufacturers='
+  );
+  const json = await data.json();
+
+  if (!json) return;
+
+  for (const product of json) {
+    const productElement = createProductElement({
+      finalPriceText: product.cashPrice,
+      rawPriceText: product.installmentPrice | product.cashPrice,
+      tag: product.alternativeTag,
+      linkText: product.merchantRedirectUrl,
+      titleText: product.name.slice(0, Math.min(product.name.length, 25)),
+    });
+
+    motherboard.appendChild(productElement);
+  }
+}
+
+async function getFilteredGPU() {
+  const data = await fetch(
+    'https://api.pcbuildwizard.com/products/video-cards/recommend?merchants=&payment=&gpuManufacturers=&chipsets=115%2C125&colors=&manufacturers='
+  );
+  const json = await data.json();
+
+  if (!json) return;
+
+  for (const product of json) {
+    const productElement = createProductElement({
+      finalPriceText: product.cashPrice,
+      rawPriceText: product.installmentPrice | product.cashPrice,
+      tag: product.alternativeTag,
+      linkText: product.merchantRedirectUrl,
+      titleText: product.name.slice(0, Math.min(product.name.length, 25)),
+    });
+
+    gpu.appendChild(productElement);
+  }
+}
+
+async function getFilteredCPU() {
+  const data = await fetch(
+    'https://api.pcbuildwizard.com/products/cpus/recommend?merchants=&payment=&useProfile=1&fpsTarget=120&sockets=AM4%2CAM5&integratedGpus=&manufacturers='
+  );
+  const json = await data.json();
+
+  if (!json) return;
+
+  for (const product of json) {
+    const productElement = createProductElement({
+      finalPriceText: product.cashPrice,
+      rawPriceText: product.installmentPrice | product.cashPrice,
+      tag: product.alternativeTag,
+      linkText: product.merchantRedirectUrl,
+      titleText: product.name.slice(0, Math.min(product.name.length, 25)),
+    });
+
+    cpu.appendChild(productElement);
+  }
+}
+
+getFilteredMotherboard();
+getFilteredGPU();
+getFilteredCPU();
